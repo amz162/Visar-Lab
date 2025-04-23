8cd5209740d88e988500f473a4dea5916070786e
amz162 <muizzsiddiqui@gmail.com>
2025-04-24 01:44:32 +0500

    header final

--- components/Header/header.jsx
+++ components/Header/header.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 
 function header() {
   return (
-    <div>
-      <nav class="bg-light-purple border-gray-200 dark:bg-light-purple">
+    <div class="border-b-3 border-b-gray-400">
+      <nav class=" bg-light-purple  dark:bg-light-purple">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="https://visar.pk/" class="flex items-center space-x-3 rtl:space-x-reverse">
             {/* <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" /> */}
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-black">Visar Labs</span>
+            <span class="font-limelight self-center text-2xl font-normal whitespace-nowrap dark:text-black">Visar Labs</span>
         </a>
         <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-            <button type="button" class="text-black bg-blue-white hover:bg-blue-white hover:text-white focus:ring-2 focus:outline-none focus:bg-tpurple focus:text-white font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-white dark:hover:bg-tpurple dark:focus:ring-purple-800">Get started</button>
+            <button type="button" class="text-black bg-blue-white hover:bg-blue-white hover:text-white focus:ring-2 focus:outline-none focus:bg-tpurple focus:text-white font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-white dark:hover:bg-tpurple dark:focus:ring-purple-800 font-kurale">Get Started</button>
             <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-purple-800 dark:text-blue-white dark:hover:bg-tpurple dark:focus:bg-tpurple dark:focus:ring-purple-800" aria-controls="navbar-cta" aria-expanded="false">
               <span class="sr-only">Open main menu</span>
               <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
@@ -20,7 +20,7 @@ function header() {
           </button>
         </div>
         <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
-          <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-light-purple md:dark:bg-light-purple dark:border-gray-700">
+          <ul class="font-kurale font-medium flex flex-col  p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-light-purple md:dark:bg-light-purple dark:border-gray-700">
             {/* <li>
               <a href="#" class="block py-2 px-3 md:p-0 text-black bg-black-700 rounded-sm md:bg-transparent md:text-black-700 md:dark:text-black-500" aria-current="page">Services</a>
             </li> */}
